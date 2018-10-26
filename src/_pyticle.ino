ef8df2c638b41d3388a90205e725af89d6eefff8
Tim Savage <tim@savage.company>
2018-10-27 02:42:31 +1100

    Added more handled messages to simulate

--- src/_pyticle.ino
+++ src/_pyticle.ino
@@ -2,13 +2,17 @@
 #include <WiFiUdp.h>
 
 #include "mqttsn_packet.h"
-#include "mqttsn_messages.h"
+#include "mqttsn_discovery.h"
+#include "mqttsn_session.h"
+#include "mqttsn_subscription.h"
+#include "mqttsn_publish.h"
 
 #define MAX_BUFFER 0x400
 #define DELAY_TIME 300000  //ms (300s)
 
 uint8_t buffer[MAX_BUFFER];
 
+MQTTSN::MessageID message_id;
 WiFiUDP udp;
 IPAddress search_ip = {192, 168, 1, 255};
 IPAddress gateway_ip;
@@ -89,10 +93,15 @@ void mqttsn_loop()
     uint8_t gateway_id;
     uint16_t duration;
 
-    uint8_t* buffer_ptr;
     uint16_t buffer_len;
+    uint8_t* message_ptr;
     uint16_t msg_len;
     uint8_t msg_type;
+    MQTTSN::Flags flags;
+    uint16_t topic_id;
+    uint16_t msg_id;
+    uint8_t* data_ptr;
+    uint16_t data_len;
 
     while (udp.parsePacket()) {
         memset(buffer, 0, MAX_BUFFER);
@@ -100,35 +109,50 @@ void mqttsn_loop()
             continue;
         }
         // Read data
-        buffer_ptr = buffer;
-        if ((msg_len = MQTTSN::read_length(&buffer_ptr, buffer_len)) == 0) {
+        message_ptr = buffer;
+        if ((msg_len = MQTTSN::read_length(&message_ptr, buffer_len)) == 0) {
             continue;
         }
-        msg_type = MQTTSN::read_uint8(&buffer_ptr);
+        msg_type = MQTTSN::read_uint8(&message_ptr);
         Serial.print("Recieved: "); Serial.print(MQTTSN::message_name(msg_type)); 
         Serial.printf(" from %s:%d\n", udp.remoteIP().toString().c_str(), udp.remotePort());
 
         switch(msg_type) {
-            case MQTTSN::MQTTSN_ADVERTISE:
-                if (!MQTTSN::unpack_advertise(&gateway_id, &duration, buffer_ptr, msg_len)) {
+            case MQTTSN::ADVERTISE:
+                if (!MQTTSN::unpack_advertise(&gateway_id, &duration, message_ptr, msg_len)) {
                     Serial.printf("Advertisement from %d: ", gateway_id); Serial.println(udp.remoteIP());
                 }
                 break;
 
-            case MQTTSN::MQTTSN_GWINFO:
+            case MQTTSN::GWINFO:
                 gateway_ip = udp.remoteIP();
                 Serial.print("Found gateway: "); Serial.println(gateway_ip);
 
                 beginMessage();
-                sendMessage(search_ip, MQTTSN::pack_connect(buffer, MAX_BUFFER, 0, DELAY_TIME, "Pyticle-Client01"));
+                sendMessage(search_ip, MQTTSN::pack_connect(buffer, MAX_BUFFER, 0, 1, 300, "Pyticle-Client01"));
                 break;
 
-            case MQTTSN::MQTTSN_CONNACK:
+            case MQTTSN::CONNACK:
+                Serial.println("Connected.");
                 nextPing = millis() + DELAY_TIME;
+
+                // Subscribe
+                beginMessage();
+                sendMessage(search_ip, MQTTSN::pack_subscribe(buffer, MAX_BUFFER, 0, 0, message_id.next(), "environmental"));
                 break;
 
-            case MQTTSN::MQTTSN_PINGRESP:
+            case MQTTSN::PINGRESP:
                 Serial.println("Received ping response...");
+                break;
+
+            case MQTTSN::SUBACK:
+                break;
+
+            case MQTTSN::PUBLISH:
+                MQTTSN::unpack_publish(&flags, &topic_id, &msg_id, &data_ptr, &data_len, message_ptr, msg_len);
+                Serial.print("Publish: topic_id="); Serial.print(topic_id); Serial.print("; msg_id="); Serial.print(msg_id);
+                Serial.print("; data_ptr="); Serial.print((char*)data_ptr); Serial.print("; data_len="); Serial.println(data_len);
+                sendMessage(search_ip, MQTTSN::pack_puback(buffer, MAX_BUFFER, topic_id, msg_id, MQTTSN::Accepted));
                 break;
 
             default:
