3ad4028fb4f5840a4597c9191474757ce61797f2
Tim Savage <tim@savage.company>
2018-10-27 12:39:40 +1100

    Basic implimentation of client class

---
+++ src/mqttsn_client.cpp
@@ -0,0 +1,105 @@
+#include <Arduino.h>
+
+#include "mqttsn_client.h"
+#include "mqttsn_packet.h"
+#include "mqttsn_discovery.h"
+#include "mqttsn_session.h"
+#include "mqttsn_subscription.h"
+
+#define MQTTSN_DEBUG
+
+
+MQTTSN::Client::Client(UDP* sock, int port)
+: m_sock(sock),
+  m_port(port),
+  m_msg_id(0)
+{
+}
+
+void MQTTSN::Client::begin(const char* client_id, IPAddress search_ip, uint32_t duration)
+{
+    m_client_id = client_id;
+    m_search_ip = search_ip;
+    m_duration = duration;
+    
+    // Initialise UDP socket
+    m_sock->begin(m_port);
+
+    // Send gateway search
+    memset(m_buffer, 0, MQTTSN_BUFFER_SIZE);
+    send_message(MQTTSN::pack_searchgw(m_buffer, MQTTSN_BUFFER_SIZE, 1), m_search_ip);
+}
+
+void MQTTSN::Client::loop()
+{
+    uint16_t buffer_len;
+    uint8_t* msg_ptr;
+    uint16_t msg_len;
+    uint8_t msg_type;
+
+    while (m_sock->parsePacket()) {
+        memset(m_buffer, 0, MQTTSN_BUFFER_SIZE);
+
+        // Read data into buffer
+        if ((buffer_len = m_sock->read(m_buffer, MQTTSN_BUFFER_SIZE)) == 0) {
+            continue;
+        }
+
+        // Parse length and message type.
+        msg_ptr = m_buffer;
+        if ((msg_len = MQTTSN::read_length(&msg_ptr, buffer_len)) == 0) {
+            continue;
+        }
+        msg_type = MQTTSN::read_uint8(&msg_ptr);
+        msg_len--;  // Remove one byte
+
+        #if defined(MQTTSN_DEBUG)
+        Serial.print("Recieved: "); Serial.print(MQTTSN::message_name(msg_type)); 
+        Serial.printf(" from %s:%d\n", m_sock->remoteIP().toString().c_str(), m_sock->remotePort());
+        #endif
+
+        switch(msg_type) {
+        case MQTTSN::GWINFO:
+            gwinfo(msg_ptr, msg_len);
+            break;
+        case MQTTSN::CONNACK:
+            connack(msg_ptr, msg_len);
+            break;
+        }
+    }
+}
+
+void MQTTSN::Client::send_message(uint16_t msg_len)
+{
+    // send_message(msg_len, m_gateway_ip);
+    send_message(msg_len, m_search_ip);
+}
+
+void MQTTSN::Client::send_message(uint16_t msg_len, IPAddress addr)
+{
+    m_sock->beginPacket(addr, m_port); 
+        m_sock->write(m_buffer, msg_len);
+    m_sock->endPacket();
+}
+
+void MQTTSN::Client::gwinfo(uint8_t* msg_ptr, uint16_t msg_len)
+{
+    m_gateway_ip = m_sock->remoteIP();
+
+    #if defined(MQTTSN_DEBUG)
+    Serial.print("Found gateway: "); Serial.println(m_gateway_ip);
+    #endif
+
+    // Initiate connection
+    memset(m_buffer, 0, MQTTSN_BUFFER_SIZE);
+    send_message(MQTTSN::pack_connect(m_buffer, MQTTSN_BUFFER_SIZE, 0, 1, m_duration, m_client_id));
+}
+
+void MQTTSN::Client::connack(uint8_t* msg_ptr, uint16_t msg_len)
+{
+    m_next_ping = millis() + m_duration;
+
+    // Subscribe
+    memset(m_buffer, 0, MQTTSN_BUFFER_SIZE);
+    send_message(MQTTSN::pack_subscribe(m_buffer, MQTTSN_BUFFER_SIZE, 0, 0, m_msg_id++, "environmental"));
+}
---
+++ src/mqttsn_client.h
@@ -0,0 +1,61 @@
+#include <Udp.h>
+
+#ifndef _mqttsn_client__h
+#define _mqttsn_client__h
+
+#ifndef MQTTSN_BUFFER_SIZE
+    #define MQTTSN_BUFFER_SIZE 0x400
+#endif //!MQTTSN_BUFFER_SIZE
+
+namespace MQTTSN 
+{
+
+    class Client
+    {
+    public:
+        /**
+         * Construct client
+         * 
+         * @param sock - UDP socket instance being used
+         * @param port - Port being used; defaults to 1883
+         */
+        Client(UDP* sock, int port=1883);
+
+        /**
+         * Begin/Initialise client
+         */
+        void begin(const char* client_id, IPAddress search_ip, uint32_t duration=300);
+
+        /**
+         * Process loop entry point.
+         */
+        void loop();
+
+    protected:
+        /**
+         * Send message packed into buffer
+         */
+        void send_message(uint16_t msg_len);
+        void send_message(uint16_t msg_len, IPAddress addr);
+
+        // Message handlers
+        void gwinfo(uint8_t* msg_ptr, uint16_t msg_len);
+        void connack(uint8_t* msg_ptr, uint16_t msg_len);
+
+    private:
+        UDP* m_sock;
+        int m_port;
+        IPAddress m_search_ip;     // IP Broadcast Address of search
+        IPAddress m_gateway_ip;    // IP Address of current gateway.
+
+        uint16_t m_msg_id;         // Next message id
+        uint32_t m_duration;       // Time (in seconds) to next ping
+        uint32_t m_next_ping;      // Time of next ping (in ms)
+
+        uint8_t m_buffer[MQTTSN_BUFFER_SIZE];
+        const char* m_client_id;
+    };
+
+} //!MQTTSN
+
+#endif //!_mqttsn_client__h
