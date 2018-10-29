f18305ed3041d269ec36ed7186fa7d6185dedfaf
Tim Savage <tim@savage.company>
2018-10-30 08:17:54 +1100

    Initial implementation of client

--- src/mqttsn_client.cpp
+++ src/mqttsn_client.cpp
@@ -4,34 +4,61 @@
 #include "mqttsn_session.h"
 #include "mqttsn_subscription.h"
 
+#if defined(MQTTSN_DEBUG)
+    #define DEBUG_print(format) MQTTSN_DEBUG_LOG.print(format)
+    #define DEBUG_printf(format, args...) MQTTSN_DEBUG_LOG.printf(format, args)
+    #define DEBUG_println(format) MQTTSN_DEBUG_LOG.println(format)
+#else
+    #define DEBUG_print(format)
+    #define DEBUG_printf(format, args...)
+    #define DEBUG_println(format)
+#endif
 
-MQTTSN::Client::Client(UDP* sock, int port)
+namespace MQTTSN {
+
+Client::Client(UDP* sock, int port)
 : m_sock(sock),
   m_port(port),
-  m_msg_id(0)
+  m_msg_id(0),
+  m_connected(false),
+  ping_in_flight(false)
 {
 }
 
-void MQTTSN::Client::begin(const char* client_id, IPAddress search_ip, uint32_t duration)
+void Client::begin(const char* client_id, IPAddress search_ip, uint32_t duration_s)
 {
     m_client_id = client_id;
     m_search_ip = search_ip;
-    m_duration = duration;
-    
+    m_duration = duration_s * 1000;
+
+    // Ensure last atom is a broadcast address
+    m_search_ip[3] = 255;
+
     // Initialise UDP socket
     m_sock->begin(m_port);
-
-    // Send gateway search
-    memset(m_buffer, 0, MQTTSN_BUFFER_SIZE);
-    send_message(MQTTSN::pack_searchgw(m_buffer, MQTTSN_BUFFER_SIZE, 1), m_search_ip);
 }
 
-void MQTTSN::Client::loop()
+void Client::loop()
+{
+    Timeout timeout(1000);
+    handle_message(timeout);
+
+    if (m_last_received.expired()) {
+        if (!ping_in_flight) {
+            ping_in_flight = true;
+
+            memset(m_buffer, 0, MQTTSN_BUFFER_SIZE);
+            send_message(pack_pingreq(m_buffer, MQTTSN_BUFFER_SIZE, m_client_id), m_search_ip);
+        }
+    }
+}
+
+MsgType Client::handle_message(Timeout& timeout)
 {
     uint16_t buffer_len;
     uint8_t* msg_ptr;
     uint16_t msg_len;
-    uint8_t msg_type;
+    MsgType msg_type;
 
     while (m_sock->parsePacket()) {
         memset(m_buffer, 0, MQTTSN_BUFFER_SIZE);
@@ -43,86 +70,159 @@ void MQTTSN::Client::loop()
 
         // Parse length and message type.
         msg_ptr = m_buffer;
-        if ((msg_len = MQTTSN::read_length(&msg_ptr, buffer_len)) == 0) {
+        if ((msg_len = read_length(&msg_ptr, buffer_len)) == 0) {
             continue;
         }
-        msg_type = MQTTSN::read_uint8(&msg_ptr);
+        msg_type = (MsgType)read_uint8(&msg_ptr);
         msg_len--;  // Remove one byte
 
-        #if defined(MQTTSN_DEBUG)
-        DEBUG_LOG.printf(
+        DEBUG_printf(
             "Recieved: %s from %s: %d\n",
-            MQTTSN::message_name(msg_type),
+            message_name(msg_type),
             m_sock->remoteIP().toString().c_str(),
             m_sock->remotePort()
         );
-        #endif
 
         switch(msg_type) {
-        case MQTTSN::ADVERTISE:
-            break;
-
-        case MQTTSN::GWINFO:
-            gwinfo(msg_ptr, msg_len);
-            break;
-
-        case MQTTSN::CONNACK:
+        case CONNACK:
             connack(msg_ptr, msg_len);
             break;
-
-        case MQTTSN::PINGRESP:
+        case REGACK:
+            regack(msg_ptr, msg_len);
+            break;
+        case SUBACK:
+            suback(msg_ptr, msg_len);
+            break;
+        case UNSUBACK:
+            unsuback(msg_ptr, msg_len);
+            break;
+        case ADVERTISE:
+            break;
+        case GWINFO:
+            gwinfo(msg_ptr, msg_len);
+            break;
+        case PINGRESP:
             pingresp(msg_ptr, msg_len);
             break;
-        
-        case MQTTSN::SUBACK:
+        case PUBLISH:
+            publish(msg_ptr, msg_len);
             break;
-        
-        case MQTTSN::PUBLISH:
-            break;
-        
-        default:
-            break;
-
         }
     }
+
+    m_last_received.expire_in(this->m_duration);
+
+    return msg_type;
 }
 
-void MQTTSN::Client::send_message(uint16_t msg_len)
+Result Client::wait_for(MsgType msg_type, Timeout& timeout)
+{
+    do {
+        if (timeout.expired()) {
+            break;
+        }
+    } while (handle_message(timeout) != msg_type);
+
+    return Success;
+}
+
+void Client::send_message(uint16_t msg_len)
 {
     // send_message(msg_len, m_gateway_ip);
     send_message(msg_len, m_search_ip);
 }
 
-void MQTTSN::Client::send_message(uint16_t msg_len, IPAddress addr)
+void Client::send_message(uint16_t msg_len, IPAddress addr)
 {
     m_sock->beginPacket(addr, m_port); 
         m_sock->write(m_buffer, msg_len);
     m_sock->endPacket();
 }
 
-void MQTTSN::Client::gwinfo(uint8_t* msg_ptr, uint16_t msg_len)
+bool Client::search()
 {
-    m_gateway_ip = m_sock->remoteIP();
+    Timeout timeout(1000);
 
-    #if defined(MQTTSN_DEBUG)
-    Serial.print("Found gateway: "); Serial.println(m_gateway_ip);
-    #endif
+    // Send gateway search
+    memset(m_buffer, 0, MQTTSN_BUFFER_SIZE);
+    send_message(pack_searchgw(m_buffer, MQTTSN_BUFFER_SIZE), m_search_ip);
+
+    if (wait_for(GWINFO, timeout) == Success) {
+        connect();
+    }
+}
+
+bool Client::connect()
+{
+    Timeout connect_timer(5000);
+    ReturnCode rc;
 
     // Initiate connection
     memset(m_buffer, 0, MQTTSN_BUFFER_SIZE);
-    send_message(MQTTSN::pack_connect(m_buffer, MQTTSN_BUFFER_SIZE, 0, 1, m_duration, m_client_id));
+    send_message(pack_connect(m_buffer, MQTTSN_BUFFER_SIZE, 0, 1, m_duration, m_client_id));
+
+    if (wait_for(CONNACK, connect_timer) == Success) {
+        unpack_connack(&rc, m_buffer + 2, MQTTSN_BUFFER_SIZE);
+    }
+
+    if (rc == Accepted) {
+        m_connected = true;
+        return true;
+    } else {
+        return false;
+    }
 }
 
-void MQTTSN::Client::connack(uint8_t* msg_ptr, uint16_t msg_len)
+//////////////////////////////////////////////////////////////////////////////
+// Message handlers
+
+uint8_t Client::gwinfo(uint8_t* msg_ptr, uint16_t msg_len)
 {
-    m_next_ping = millis() + m_duration;
-
-    // Subscribe
-    memset(m_buffer, 0, MQTTSN_BUFFER_SIZE);
-    send_message(MQTTSN::pack_subscribe(m_buffer, MQTTSN_BUFFER_SIZE, 0, QoSLevel0, m_msg_id++, "environmental"));
+    m_gateway_ip = m_sock->remoteIP();
+    DEBUG_print("Found gateway: "); DEBUG_println(m_gateway_ip);
+    return Success;
 }
 
-void MQTTSN::Client::pingresp(uint8_t* msg_ptr, uint16_t msg_len)
+uint8_t Client::connack(uint8_t* msg_ptr, uint16_t msg_len)
+{
+    return Success;
+}
+
+uint8_t Client::regack(uint8_t* msg_ptr, uint16_t msg_len)
 {
 
+    return Success;
 }
+
+uint8_t Client::publish(uint8_t* msg_ptr, uint16_t msg_len)
+{
+
+    return Success;
+}
+
+uint8_t Client::puback(uint8_t* msg_ptr, uint16_t msg_len)
+{
+
+    return Success;
+}
+
+uint8_t Client::suback(uint8_t* msg_ptr, uint16_t msg_len)
+{
+
+    return Success;
+}
+
+uint8_t Client::unsuback(uint8_t* msg_ptr, uint16_t msg_len)
+{
+
+    return Success;
+}
+
+uint8_t Client::pingresp(uint8_t* msg_ptr, uint16_t msg_len)
+{
+    ping_in_flight = false;
+    DEBUG_print("Ping received: "); DEBUG_println(m_gateway_ip);
+    return Success;
+}
+
+};
--- src/mqttsn_client.h
+++ src/mqttsn_client.h
@@ -1,18 +1,78 @@
-#include <Arduino.h>
-#include <Udp.h>
-
 #ifndef _mqttsn_client__h
 #define _mqttsn_client__h
 
-#ifndef MQTTSN_BUFFER_SIZE
-    #define MQTTSN_BUFFER_SIZE 0x400
-#endif //!MQTTSN_BUFFER_SIZE
+#include <Arduino.h>
+#include <Udp.h>
+#include "mqttsn_packet.h"
 
 #define MQTTSN_DEBUG
-#define DEBUG_LOG Serial
+#define MQTTSN_DEBUG_LOG Serial
+
+
+#if !defined(MQTTSN_BUFFER_SIZE)
+    #define MQTTSN_BUFFER_SIZE 128
+#endif
+
+#if !defined(MQTTSN_MAX_REGISTRATIONS)
+    #define MQTTSN_MAX_REGISTRATIONS 4
+#endif
+#if !defined(MQTTSN_MAX_REGISTRATION_TOPIC_LEN)
+    #define MQTTSN_MAX_REGISTRATION_TOPIC_LEN 32
+#endif
+
+#if !defined(MQTTSN_MAX_PING_RETRIES)
+    #define MQTTSN_MAX_PING_RETRIES 3
+#endif
 
 namespace MQTTSN 
 {
+
+    class Timeout
+    {
+        public:
+            Timeout(unsigned long ms=0) { m_expiry = ms ? (millis() + ms) : 0; }
+        
+            // Has the timerout expired?
+            bool expired() { 
+                if (m_expiry == 0) {
+                    return false;
+                } 
+                return m_expiry < millis(); 
+            }
+
+            // Is the timeout active?
+            bool active() { return m_expiry != 0; }
+            
+            // How much time is left?
+            unsigned long left_ms()
+            {
+                unsigned long now = millis();
+                if (m_expiry < now) {
+                    return 0;
+                } else {
+                    return m_expiry - now;
+                }
+            }
+            
+            // Please expire in...
+            void expire_in(unsigned long ms) { m_expiry = millis() + ms; }
+            
+            // Please expire at...
+            void expire_at(unsigned long ms) { m_expiry = ms; }
+
+        private:
+            unsigned long m_expiry;
+    };
+
+
+    /**
+     * Result structure
+     */
+    enum Result : uint8_t {
+        Success,
+        Failure = (uint8_t)-1,
+    };
+
 
     class Client
     {
@@ -30,11 +90,46 @@ namespace MQTTSN
          */
         void begin(const char* client_id, IPAddress search_ip, uint32_t duration=300);
 
-        void connect();
+    protected:
+        /**
+         * Handle Message
+         */
+        MsgType handle_message(Timeout& timeout);
+
+        /**
+         * Wait for a particular a message
+         */
+        Result wait_for(MsgType msg_type, Timeout& timeout);
+        
+        /**
+         * Send message packed into buffer
+         */
+        void send_message(uint16_t msg_len);
+        void send_message(uint16_t msg_len, IPAddress addr);
+
+    public:
+        /**
+         * Search for gateway
+         */
+        bool search();
+
+        /**
+         * Start connect
+         */
+        bool connect();
+        void sleep();
+        void awake();
+        void disconnect();
 
         uint16_t register_topic(const char* topic_name);
+        void unregister_topic(uint16_t topic_id);
+        void unregister_all_topics();
 
-        void publish();
+        uint16_t subscribe(const char* topic_name);
+        void unsubscribe(const char* topic_name);
+
+        void publish(const char* topic_name, void* data, uint16_t data_len, enum QosLevel qos=QoSLevel0, bool retained=false);
+        void publish(uint16_t topic_id, void* data, uint16_t data_len, enum QosLevel qos=QoSLevel0, bool retained=false);
 
         /**
          * Process loop entry point.
@@ -42,26 +137,32 @@ namespace MQTTSN
         void loop();
 
     protected:
-        /**
-         * Send message packed into buffer
-         */
-        void send_message(uint16_t msg_len);
-        void send_message(uint16_t msg_len, IPAddress addr);
-
         // Message handlers
-        void gwinfo(uint8_t* msg_ptr, uint16_t msg_len);
-        void connack(uint8_t* msg_ptr, uint16_t msg_len);
-        void pingresp(uint8_t* msg_ptr, uint16_t msg_len);
+        uint8_t gwinfo(uint8_t* msg_ptr, uint16_t msg_len);
+        uint8_t connack(uint8_t* msg_ptr, uint16_t msg_len);
+        uint8_t regack(uint8_t* msg_ptr, uint16_t msg_len);
+        uint8_t publish(uint8_t* msg_ptr, uint16_t msg_len);
+        uint8_t puback(uint8_t* msg_ptr, uint16_t msg_len);
+        uint8_t suback(uint8_t* msg_ptr, uint16_t msg_len);
+        uint8_t unsuback(uint8_t* msg_ptr, uint16_t msg_len);
+        uint8_t pingresp(uint8_t* msg_ptr, uint16_t msg_len);
 
     private:
+        uint32_t m_duration;       // Time (in seconds) to next ping
+
         UDP* m_sock;
         int m_port;
         IPAddress m_search_ip;     // IP Broadcast Address of search
         IPAddress m_gateway_ip;    // IP Address of current gateway.
+        bool m_connected;
+
+        uint8_t ping_retry;
+        bool ping_in_flight;
+
+        Timeout m_last_received;     // Last time a message was received
+        Timeout m_next_ping;         // Last time a message was received
 
         uint16_t m_msg_id;         // Next message id
-        uint32_t m_duration;       // Time (in seconds) to next ping
-        uint32_t m_next_ping;      // Time of next ping (in ms)
 
         uint8_t m_buffer[MQTTSN_BUFFER_SIZE];
         const char* m_client_id;
